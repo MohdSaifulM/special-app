5e53898e526b79a73ffb73aca61d40a3e8f31ba0
Schnecke <eiweala@live.com>
2020-11-13 13:50:00 +0800

    start up files

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const name = "siu sing";
+require("dotenv").config();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
-const name3= "Ebere123455454544";
+const app = express();
 
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
+app.use("/auth", require("./routes/auth.routes"));
+
+app.listen(process.env.PORT, () =>
+  console.log(`running on ${process.env.PORT}`)
+);
