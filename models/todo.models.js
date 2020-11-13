7018e9052a228e44f720818dab4af580c4aa120b
Schnecke <eiweala@live.com>
2020-11-13 15:41:07 +0800

    add validation

--- models/todo.models.js
+++ models/todo.models.js
@@ -1,27 +1,28 @@
-const moment = require("moment")
-const mongoose = require('mongoose')
-const {
-    Schema
-} = mongoose;
+const moment = require("moment");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const todoSchema = new Schema({
+const todoSchema = new Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-}, {
-    timestamps: true
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 //Called from document
 todoSchema.methods.fromNow = function () {
-    return `${moment().format(this.createdAt).fromNow()}`
-}
+  return `${moment(this.createdAt).fromNow()}`;
+};
 
-const Todo = mongoose.model("Todo", todoSchema)
+const Todo = mongoose.model("Todo", todoSchema);
 
 module.exports = Todo;
